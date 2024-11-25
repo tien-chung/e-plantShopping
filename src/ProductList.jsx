f74c6434c23dac978018ba0930fc133096772547
Tien Chung <tchung7171@gmail.com>
2024-11-25 17:15:44 -0500

    Updated addToCart onClick

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,8 +292,8 @@ const handlePlantsClick = (e) => {
                             <img className="product-image" src={plant.image} alt={plant.name} />
                             <div className="product-price">{plant.cost}</div>
                             <div>{plant.description}</div>
-                            <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
-                        </div>
+                            <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`} onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.name]}>{addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}</button>
+                            </div>
                         ))}
                     </div>
                 </div>
